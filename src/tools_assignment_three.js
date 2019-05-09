95ee861622e51bc49acfe87711eb80e1a4011430
tanvidomadia <tanvidomadia15@gmail.com>
2019-05-09 15:10:36 -0400

    adding here

--- src/tools_assignment_three.js
+++ src/tools_assignment_three.js
@@ -109,10 +109,10 @@ h = results;
 		 this.game.scale.refresh();
 		 this.game.stage.backgroundColor = "#ffffff";
 	 style2 = { font: "14px arial", fill: "#000000", boundsAlignH: "center", boundsAlignV: "middle" };
-
-	 left_button[0] = game.add.sprite(30,235,'after');
-	 left_button[0].scale.setTo(0.08,0.08);
-	 left_button[0].inputEnabled = true;
+	 //
+	 // left_button[0] = game.add.sprite(30,235,'after');
+	 // left_button[0].scale.setTo(0.08,0.08);
+	 // left_button[0].inputEnabled = true;
 	 //left_button[0].events.onInputDown.add(this.backtosecondscsreen,this);
 	 var style = { font: "23px arial", fill: "#000000", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 850 };
 	 //displaying questions for mcq
@@ -176,10 +176,10 @@ h = results;
 			 }
 
 
-		 screen_text[1] = game.add.text(92,30,question,style);
+		 screen_text[1] = game.add.text(30,30,question,style);
 		 var style1 = { font: "20px tahoma", fill: "#000000", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 850};
 
-		 screen_text[2] = game.add.text(95,200,'Click on the correct text answer below.',style1);
+		 screen_text[2] = game.add.text(30,400,'Click on the correct text answer below.',style1);
 		 var style2 = { font: "23px tahoma", fill: "#737373", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 850 };
 
 			//  for (i=1;i<h.data.length;i++)
@@ -195,86 +195,65 @@ h = results;
 		 //
 		 // }
 		 //adding all answers for a question in one text
-		 if(score_for_each_question == 0)
-		 {
-		 answer_option[0] = game.add.text(100,240,'A.' + answer_pool1[0],style2);
-		 answer_option[1] = game.add.text(100,400,'B.' + answer_pool2[0],style2);
-		 answer_option[2] = game.add.text(100,560,'C.' + answer_pool3[0],style2);
-		 answer_option[3] = game.add.text(100,720,'D.' + answer_pool4[0],style2);
-		 for(i=0;i<4;i++)
-		 {
-		 answer_option[i].inputEnabled = true;
-		 answer_option[i].events.onInputDown.add(this.feedback_function,this);
-
-		 console.log('hey');
-		 }
- }
-	 if (turn ==1)
-	 {
-		 {
-		answer_option[0] = game.add.text(100,240,'A.' + answer_pool1[1],style2);
-		answer_option[1] = game.add.text(100,400,'B.' + answer_pool2[1],style2);
-		answer_option[2] = game.add.text(100,560,'C.' + answer_pool3[1],style2);
-		answer_option[3] = game.add.text(100,720,'D.' + answer_pool4[1],style2);
-	 }
-	 for(i=0;i<4;i++)
-	 {
-	 answer_option[i].inputEnabled = true;
-	 answer_option[i].events.onInputDown.add(this.feedback_function1,this);
-
-	 }
-
-	 }
-	 else
-	 {
-		 count = count + 1;
-		 game.state.start('start_screen');
-	 }
-
-
+		 // timer
+		 // answer_option[0] = game.add.text(100,240,'A.' + answer_pool1[0],style2);
+		 // answer_option[1] = game.add.text(100,400,'B.' + answer_pool2[0],style2);
+		 // answer_option[2] = game.add.text(100,560,'C.' + answer_pool3[0],style2);
+		 // answer_option[3] = game.add.text(100,720,'D.' + answer_pool4[0],style2);
+		 // for(i=0;i<4;i++)
+		 // {
+		 // answer_option[i].inputEnabled = true;
+		 // answer_option[i].events.onInputDown.add(this.feedback_function,this);
 		 //
-		 for(i=0;i<4;i++)
-		 {
-		 answer_option[i].inputEnabled = true;
-		 //answer_option[i].events.onInputDown.add(this.feedback_function,this);
-		 //answer_option[i].events.onInputOver.add(over, this);
-		 //answer_option[i].text.events.onInputOut.add(out, this);
-		 console.log('hey');
-		 }
+		 // console.log('hey');
+		 // }
+
+
+
+
+		 // //
+		 // for(i=0;i<4;i++)
+		 // {
+		 // answer_option[i].inputEnabled = true;
+		 // //answer_option[i].events.onInputDown.add(this.feedback_function,this);
+		 // //answer_option[i].events.onInputOver.add(over, this);
+		 // //answer_option[i].text.events.onInputOut.add(out, this);
+		 // console.log('hey');
+		 // }
 	 }
 
-	 //displaying questions for Sa -- converting to mcq
-	 else
-	 {
-		 console.log('hi1');
-		 screen_text[1] = game.add.text(92,146,h.data[count][2],style);
-		 var style1 = { font: "20px tahoma", fill: "#000000", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 800 };
-
-		 screen_text[2] = game.add.text(95,190,'Click on the correct text answer below.',style1);
-		 var style2 = { font: "23px tahoma", fill: "#737373", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 800 };
-
-		 answer_option[0] = game.add.text(100,230,'A.' + g.data[count][0],style2);
-
-		 answer_option[1] = game.add.text(100,260,'B.' + g.data[count][0],style2);
-		 answer_option[2] = game.add.text(100,290,'C.' + g.data[count][0],style2);
-		 answer_option[3] = game.add.text(100,320,'D.' + g.data[count][0],style2);
-		 style3 = { font: "23px tahoma", fill: "#39ff14", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 800}; //correctanswer
-		 feedback[0] = game.add.text(95,502,'',style3);
-		 style4 = { font: "23px tahoma", fill: "#FF0000", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 800 };//wrong answer
-		 feedback_next[0]=game.add.text(95,530,'',style3);
-		 feedback[1] = game.add.text(95,502,'',style4);
-		 feedback_next[1]=game.add.text(95,530,'',style4);
-		 //
-		 for(i=0;i<4;i++)
-		 {
-		 answer_option[i].inputEnabled = true;
-		 //answer_option[i].events.onInputDown.add(this.feedback_function,this);
-		 //answer_option[i].events.onInputOver.add(over, this);
-		 //answer_option[i].text.events.onInputOut.add(out, this);
-		 console.log('hey');
-
-		 }
-	 }
+	 // //displaying questions for Sa -- converting to mcq
+	 // else
+	 // {
+		//  console.log('hi1');
+		//  screen_text[1] = game.add.text(92,146,h.data[count][2],style);
+		//  var style1 = { font: "20px tahoma", fill: "#000000", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 800 };
+	 //
+		//  screen_text[2] = game.add.text(95,190,'Click on the correct text answer below.',style1);
+		//  var style2 = { font: "23px tahoma", fill: "#737373", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 800 };
+	 //
+		//  answer_option[0] = game.add.text(100,230,'A.' + g.data[count][0],style2);
+	 //
+		//  answer_option[1] = game.add.text(100,260,'B.' + g.data[count][0],style2);
+		//  answer_option[2] = game.add.text(100,290,'C.' + g.data[count][0],style2);
+		//  answer_option[3] = game.add.text(100,320,'D.' + g.data[count][0],style2);
+		//  style3 = { font: "23px tahoma", fill: "#39ff14", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 800}; //correctanswer
+		//  feedback[0] = game.add.text(95,502,'',style3);
+		//  style4 = { font: "23px tahoma", fill: "#FF0000", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 800 };//wrong answer
+		//  feedback_next[0]=game.add.text(95,530,'',style3);
+		//  feedback[1] = game.add.text(95,502,'',style4);
+		//  feedback_next[1]=game.add.text(95,530,'',style4);
+		//  //
+		//  for(i=0;i<4;i++)
+		//  {
+		//  answer_option[i].inputEnabled = true;
+		//  //answer_option[i].events.onInputDown.add(this.feedback_function,this);
+		//  //answer_option[i].events.onInputOver.add(over, this);
+		//  //answer_option[i].text.events.onInputOut.add(out, this);
+		//  console.log('hey');
+	 //
+		//  }
+	 // }
 	 // }, 100);
 	 console.log(final_array);
 }
@@ -355,6 +334,23 @@ h = results;
 	{
 
 	},
+	update : function()
+	{
+		if(answer_pool1.length > 2)
+		{
+		answer_option[0] = game.add.text(100,240,'A.' + answer_pool1[0],style2);
+		answer_option[1] = game.add.text(100,400,'B.' + answer_pool2[0],style2);
+		answer_option[2] = game.add.text(100,560,'C.' + answer_pool3[0],style2);
+		answer_option[3] = game.add.text(100,720,'D.' + answer_pool4[0],style2);
+		for(i=0;i<4;i++)
+		{
+		answer_option[i].inputEnabled = true;
+		answer_option[i].events.onInputDown.add(this.feedback_function,this);
+
+		console.log('hey');
+		}
+	}
+},
 }
 var second_screen = function(game){}
 second_screen.prototype =
@@ -396,115 +392,7 @@ start : function()
 	 // {
 	// setTimeout(function () {
 
-		if(turn == 0)
-		{
-		console.log(g);
-		console.log(h);
-		this.game.scale.pageAlignHorizontally = true;
-		this.game.scale.pageAlignVertically = true;
-		this.game.scale.refresh();
-		this.game.stage.backgroundColor = "#ffffff";
-	style2 = { font: "14px arial", fill: "#000000", boundsAlignH: "center", boundsAlignV: "middle" };
 
-	left_button[0] = game.add.sprite(30,235,'after');
-	left_button[0].scale.setTo(0.08,0.08);
-	left_button[0].inputEnabled = true;
-	//left_button[0].events.onInputDown.add(this.backtosecondscsreen,this);
-	var style = { font: "23px arial", fill: "#000000", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 850 };
-	//displaying questions for mcq
-		if( h.data[count][0] == "MC (Multiple choice)" || h.data[count][0] == "MC")
-		{
-			// if (score_for_each_question < 1)
-			{
-				question = h.data[count][2];
-				final_array[0] = question;
-
-				console.log(question);
-				answer_pool1[0] = h.data[count][4];
-				answer_pool2[0] = h.data[count][6];
-				answer_pool3[0] = h.data[count][8];
-				answer_pool4[0] = h.data[count][10];
-				correct_ans = h.data[count][3];
-				 question_id = h.data[count][1];
-				final_array[1] = question_id;
-				final_array[2] = correct_ans;
-			 // final_array[3] =
-			}
-			//find the max average score
-			var max = [];
-			console.log(g.data);
-			delete g.data[0];
-		 // g.data = sortJSON(g.data, 'Question_id','123');
-		 //g.data = _.sortBy(g.data,2);
-			g.data =  _.sortBy(g.data);
-			//console.log(g.data);
-			console.log(g.data);
-			// for(j=0;j<4;j++)
-			// {
-			// max[j] = 0;
-			 // }
-			//parsing the answer pool
-			for(i=0;i<g.data.length-1;i++)
-			{
-				if(g.data[i][1] == h.data[count][1]) //matching question ids
-				{
-					if(g.data[i][3] == "A") //
-					{
-						//max =  g.data[i][4];
-						answer_pool1.push(g.data[i][0]);
-					}
-					if(g.data[i][3] == "B") //g.data[i][4] > max[1] &&
-					{
-						//max =  g.data[i][4];
-						answer_pool2.push(g.data[i][0]);
-					}
-					if(g.data[i][3] == "C") //g.data[i][4] > max[2]
-					{
-						//max[2] =  g.data[i][4];
-						answer_pool3.push(g.data[i][0]);
-					}
-					if(g.data[i][3] == "D") //g.data[i][4] > max[3] &&
-					{
-					 // max[3] = g.data[i][4];
-						answer_pool4.push(g.data[i][0]);
-					}
-				}
-			}
-
-
-		screen_text[1] = game.add.text(92,30,question,style);
-		var style1 = { font: "20px tahoma", fill: "#000000", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 850};
-
-		screen_text[2] = game.add.text(95,200,'Click on the correct text answer below.',style1);
-		var style2 = { font: "23px tahoma", fill: "#737373", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 850 };
-
-		 //  for (i=1;i<h.data.length;i++)
-		 //  {
-		 //   question_pool[i][1] = h.data[][];
-		 // 	question_pool[i][1] = score;
-		 // 	// //adding the question options in the first aloop
-		 // }
-
-		//
-		//
-		 // //adding the student generated responses in the answer pool
-		//
-		// }
-		//adding all answers for a question in one text
-		if(score_for_each_question == 0)
-		{
-		answer_option[0] = game.add.text(100,240,'A.' + answer_pool1[0],style2);
-		answer_option[1] = game.add.text(100,400,'B.' + answer_pool2[0],style2);
-		answer_option[2] = game.add.text(100,560,'C.' + answer_pool3[0],style2);
-		answer_option[3] = game.add.text(100,720,'D.' + answer_pool4[0],style2);
-		for(i=0;i<4;i++)
-		{
-		answer_option[i].inputEnabled = true;
-		answer_option[i].events.onInputDown.add(this.feedback_function,this);
-
-		console.log('hey');
-		}
-}
 	if (turn ==1)
 	{
 		{
@@ -537,11 +425,9 @@ start : function()
 		//answer_option[i].text.events.onInputOut.add(out, this);
 		console.log('hey');
 		}
-	}
 
-	//displaying questions for Sa -- converting to mcq
-	else
-	{
+
+
 		console.log('hi1');
 		screen_text[1] = game.add.text(92,146,h.data[count][2],style);
 		var style1 = { font: "20px tahoma", fill: "#000000", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 800 };
@@ -570,10 +456,10 @@ start : function()
 		console.log('hey');
 
 		}
-	}
+
 	// }, 100);
 	console.log(final_array);
-}
+
  },
  feedback_function : function(item)
  {
